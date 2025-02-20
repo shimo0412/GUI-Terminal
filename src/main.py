09588cd58a0f8a2fcc56dff9dddac72d8cdd07e0
shimo0412 <shimo_0412@outlook.jp>
2025-02-20 22:38:15 +0900

    Delete src directory

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-from terminal_ui import TerminalApp
-import sys
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = TerminalApp()
-    window.show()
-    sys.exit(app.exec())
